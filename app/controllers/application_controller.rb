844d1d3ac2fecf64b43cae5202206f90d0aeea7b
Najma <najmathande@gmail.com>
2023-07-02 16:35:23 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
-class ApplicationController < ActionController::API
-  include ActionController::Cookies
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user
 
+  private
+
+  def authenticate_user
+    head :unauthorized unless current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
